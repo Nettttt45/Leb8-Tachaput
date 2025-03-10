1b0368587f92ccfebdde32ce03111e547a0a0e0b
Nettttt45 <netnadee@gmail.com>
2025-03-10 10:11:52 +0700

    Initial project

---
+++ Assets/Gravity.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    Rigidbody rb;
+    const float G = 0.00674f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+    void Attract(Gravity other)
+    {
+        Rigidbody rbOther = GetComponent<Rigidbody>();
+
+        Vector3 direction = rb.position - rbOther.position;
+
+        float distance = direction.magnitude;
+
+        if (distance < 0) { return; }
+
+        float forceMagnitude = G * ((rb.mass * other.rb.mass)/ Mathf.Pow(distance, 2));
+
+        Vector3 force = forceMagnitude * direction.normalized;
+    }
+}
